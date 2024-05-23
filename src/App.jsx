b2cd822f4290e028ef129b4c8a15eab4befba1dc
Arsen Shulika <arsenshulika@gmail.com>
2024-05-23 22:44:08 +0200

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Description from './components/Description/Description.jsx';
+import Options from './components/Options/Options.jsx';
+import Feedback from './components/Feedback/Feedback.jsx';
+
+export default function App() {
+  return (
+    <>
+      <Description />
+      <Options />
+      <Feedback />
+    </>
+  );
+}
