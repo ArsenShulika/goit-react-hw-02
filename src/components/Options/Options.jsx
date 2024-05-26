df4068b933c1ff2ef5d48b4296ac1ae95f4bd0c1
Arsen Shulika <arsenshulika@gmail.com>
2024-05-26 19:32:37 +0200

    first rewiev

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,19 @@
+import css from './Options.module.css';
+const Options = ({ feedback, setFeedback, updateFeedback }) => {
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  return (
+    <div className={css.container}>
+      <button onClick={() => updateFeedback('good')}>Good</button>
+      <button onClick={() => updateFeedback('neutral')}>Neutral</button>
+      <button onClick={() => updateFeedback('bad')}>Bad</button>
+      {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
+    </div>
+  );
+};
+
+export default Options;
